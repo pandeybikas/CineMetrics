c8ed55c09ea359afddf78b52858771296afa6521
bikas pandey <bikaspandey92@gmail.com>
2026-07-27 18:24:45 +0530

    created docker file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -56,8 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS =[
-    "http://localhost:4200",
-    "http://127.0.0.1:4200",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000"
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from rest_framework import status
 
 def healthcheck(request):
-    return HttpResponse({'msg': 'ok'}, status=status.HTTP_200_OK)
+    return JsonResponse({'msg': 'ok'}, status=status.HTTP_200_OK)
